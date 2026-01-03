1b88ee7ead913236dbdca9e6335f167428dd47ec
Ncticn <nctiicn@gmail.com>
2026-01-03 10:35:33 +0300

    LogoBrand Component Created and Completed.

---
+++ 22-pod-request-access-landing-page/src/components/LogoBrand.tsx
@@ -0,0 +1,11 @@
+export default function LogoBrand() {
+  return (
+    <div className="brand-logo inline-block">
+      <img
+        src="/logo.svg"
+        alt="Pod Logo"
+        className="max-w-full h-auto object-contain"
+      />
+    </div>
+  );
+}
