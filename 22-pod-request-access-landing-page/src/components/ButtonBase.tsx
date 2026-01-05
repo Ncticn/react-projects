da8613d96e1f70d7f3a9a936fb2e4f5fb39de7ca
Ncticn <nctiicn@gmail.com>
2026-01-05 17:12:03 +0300

    The “ButtonBase” component has been restyled for use with “InputEmail” and for the hover effect.

--- 22-pod-request-access-landing-page/src/components/ButtonBase.tsx
+++ 22-pod-request-access-landing-page/src/components/ButtonBase.tsx
@@ -12,7 +12,7 @@ export default function ButtonBase({
   return (
     <button
       type={type}
-      className="relative overflow-hidden rounded-[28px] px-8 py-2 cursor-pointer flex items-center justify-center gap-2 bg-green-base text-blue-950 text-preset-3 before:absolute before:inset-0 before:bg-white/50 before:opacity-0 before:transition-opacity before:mix-blend-overlay before:duration-200 z-10 before:z-0 hover:before:opacity-100"
+      className="rounded-[28px] px-8 py-2 overflow-hidden cursor-pointer flex items-center justify-center gap-2 bg-green-base text-blue-950 text-preset-3  not-md:w-full md:absolute not-md:inset-x-0 md:right-1 md:top-1 md:bottom-1 before:absolute md:before:inset-0 before:bg-white/50 before:opacity-0 before:transition-opacity before:mix-blend-overlay before:duration-200 z-10 before:z-0 hover:before:opacity-100"
     >
       {children}
     </button>
