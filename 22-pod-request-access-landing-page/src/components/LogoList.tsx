3e1d013ea174f93135d9f045a16f201434ddc116
Ncticn <nctiicn@gmail.com>
2026-01-05 16:41:30 +0300

    The sizes of the logos in the Logo List have been made responsive.

--- 22-pod-request-access-landing-page/src/components/LogoList.tsx
+++ 22-pod-request-access-landing-page/src/components/LogoList.tsx
@@ -6,16 +6,21 @@ type Logo = {
 
 interface LogoListProps {
   logoItems: Logo[];
+  className?: string;
 }
-function LogoList({ logoItems }: LogoListProps) {
+function LogoList({ logoItems, className }: LogoListProps) {
   return (
-    <ul className="md:pt-6 flex items-center justify-between md:justify-start md:gap-10 w-fit">
+    <ul
+      className={`${
+        className ? className : ""
+      } md:pt-6 flex items-center justify-between md:justify-start md:gap-10 w-full`}
+    >
       {logoItems.map((logo) => (
         <li key={logo.id}>
           <img
             src={logo.url}
             alt="Sponsor Brand Logo"
-            className="max-w-fit h-auto object-contain"
+            className="object-contain max-w-14 md:max-w-full"
           />
         </li>
       ))}
