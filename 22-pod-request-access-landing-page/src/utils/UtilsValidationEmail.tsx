6e233024d1d48f36e67360059b1cec7928ef0ec8
Ncticn <nctiicn@gmail.com>
2026-01-05 17:07:37 +0300

    A file named “UtilsValidationEmail” was created. A function was written that validates the email and then returns the values.

---
+++ 22-pod-request-access-landing-page/src/utils/UtilsValidationEmail.tsx
@@ -0,0 +1,15 @@
+function UtilsValidationEmail(email: string) {
+  const errors: string[] = [];
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  if (email.length === 0) errors.push("Email cannot be empty.");
+  if (!emailRegex.test(email.trim()))
+    errors.push("Oops! Please check your email");
+
+  return {
+    isValid: errors.length === 0,
+    error: errors[0],
+  };
+}
+
+export default UtilsValidationEmail;
