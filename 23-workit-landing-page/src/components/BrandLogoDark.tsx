57a800c44b526e93133f5ef4b41c8da76cd0b121
Ncticn <nctiicn@gmail.com>
2026-01-06 16:35:02 +0300

    LogoBrand Components Created and Completed.

---
+++ 23-workit-landing-page/src/components/BrandLogoDark.tsx
@@ -0,0 +1,8 @@
+function BrandLogoDark() {
+  return (
+    <figure className="inline-block">
+      <img src="/logo-dark.svg" alt="Brand Logo" className="object-contain" />
+    </figure>
+  );
+}
+export default BrandLogoDark;
---
+++ 23-workit-landing-page/src/components/BrandLogoWhite.tsx
@@ -0,0 +1,8 @@
+function BrandLogoWhite() {
+  return (
+    <figure className="inline-block">
+      <img src="/logo-light.svg" alt="Brand Logo" className="object-contain" />
+    </figure>
+  );
+}
+export default BrandLogoWhite;
