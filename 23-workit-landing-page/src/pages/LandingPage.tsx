c31c574fb5c3ea97af3e4ef8d00eb3aebc417851
Ncticn <nctiicn@gmail.com>
2026-01-07 15:25:30 +0300

    The LandingPage Component has been created and its design has been customized. The “ornamental” visual has been added.

---
+++ 23-workit-landing-page/src/pages/LandingPage.tsx
@@ -0,0 +1,29 @@
+import HeaderBase from "../components/HeaderBase";
+import SectionCTA from "../components/SectionCTA";
+import SectionFeature from "../components/SectionFeatures";
+import SectionHero from "../components/SectionHero";
+
+export default function LandingPage() {
+  return (
+    <>
+      <div className="bg-purple-900">
+        <HeaderBase />
+        <SectionHero />
+        <img
+          src="src/assets/patterns/ornamental.svg"
+          alt="Background Pattern Image"
+          className="w-full min-h-10 object-cover"
+        />
+      </div>
+      <div className="relative h-20 md:h-50">
+        <img
+          src="src/assets/images/image-hero.webp"
+          alt="Section Hero Image"
+          className="object-contain max-w-75 md:max-w-lg lg:max-w-2xl absolute -top-24 right-1/12  md:-top-32 md:right-1/6 lg:-top-62 lg:right-1/3"
+        />
+      </div>
+      <SectionFeature />
+      <SectionCTA />
+    </>
+  );
+}
