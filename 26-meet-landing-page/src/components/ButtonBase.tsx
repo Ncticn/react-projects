c4c20fa442156809e81471c55caaf5850b7a6a9c
Ncticn <nctiicn@gmail.com>
2026-01-12 20:27:51 +0300

    ButtonBase component has been created and completed.

---
+++ 26-meet-landing-page/src/components/ButtonBase.tsx
@@ -0,0 +1,35 @@
+type ButtonBaseProps = {
+  buttonColor: "purple" | "blue";
+  text: string;
+  extraText?: string;
+};
+
+export default function ButtonBase({
+  buttonColor,
+  extraText,
+  text,
+}: ButtonBaseProps) {
+  let customClass = "";
+  if (buttonColor == "blue") customClass = "bg-cyan-600 hover:bg-[#71C0DA]";
+  else if (buttonColor == "purple")
+    customClass = "bg-purple-600 hover:bg-[#B18BDD]";
+
+  return (
+    <a
+      href="/"
+      aria-label="Click to downloading"
+      className={`text-preset-5 text-white-base inline-flex flex-row items-start justify-start gap-2 rounded-[29px] px-10 py-4 transition-colors delay-100 ease-linear ${customClass}`}
+    >
+      {text && <span>{text}</span>}
+      {extraText && (
+        <span
+          className={
+            buttonColor == "blue" ? "text-cyan-300" : "text-purple-300"
+          }
+        >
+          {extraText}
+        </span>
+      )}
+    </a>
+  );
+}
