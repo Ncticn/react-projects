640d89364b1b90acefc683cc742156cd20e1aef0
Ncticn <nctiicn@gmail.com>
2026-01-12 20:10:16 +0300

    The BrandLogo component has been created. The Brand Logo Image has been added.

---
+++ 26-meet-landing-page/src/components/BrandLogo.tsx
@@ -0,0 +1,7 @@
+export default function BrandLogo() {
+  return (
+    <a href="/" aria-label="Go to Homepage" className="inline-block">
+      <img src="/logo.svg" alt="Brand Logo" />
+    </a>
+  );
+}
