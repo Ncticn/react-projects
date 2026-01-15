7c90ff73d23ac47f61188c14b8ba513bf7a53d47
Ncticn <nctiicn@gmail.com>
2026-01-15 12:47:19 +0300

    LandingPage Sayfası oluşturuldu. SectionHero ve SectionFeatures bileşenleri eklendi.

---
+++ 26-meet-landing-page/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import SectionFeatures from "../components/SectionFeatures";
+import SectionHero from "../components/SectionHero";
+
+function LandingPage() {
+  return (
+    <>
+      <SectionHero />
+      <SectionFeatures />
+    </>
+  );
+}
+
+export default LandingPage;
