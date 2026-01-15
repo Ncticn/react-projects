397adae495ec8f4ba56e51e8511d99ad0929f6e0
Ncticn <nctiicn@gmail.com>
2026-01-15 12:43:20 +0300

    The className property has been added to the ButtonBase component.

--- 26-meet-landing-page/src/components/ButtonBase.tsx
+++ 26-meet-landing-page/src/components/ButtonBase.tsx
@@ -2,12 +2,14 @@ type ButtonBaseProps = {
   buttonColor: "purple" | "blue";
   text: string;
   extraText?: string;
+  className?: string;
 };
 
 export default function ButtonBase({
   buttonColor,
   extraText,
   text,
+  className,
 }: ButtonBaseProps) {
   let customClass = "";
   if (buttonColor == "blue") customClass = "bg-cyan-600 hover:bg-[#71C0DA]";
@@ -18,7 +20,7 @@ export default function ButtonBase({
     <a
       href="/"
       aria-label="Click to downloading"
-      className={`text-preset-5 text-white-base inline-flex flex-row items-start justify-start gap-2 rounded-[29px] px-10 py-4 transition-colors delay-100 ease-linear ${customClass}`}
+      className={`text-preset-5 text-white-base inline-flex flex-row items-start justify-start gap-2 rounded-[29px] px-10 py-4 transition-colors delay-100 ease-linear ${customClass} ${className}`}
     >
       {text && <span>{text}</span>}
       {extraText && (
