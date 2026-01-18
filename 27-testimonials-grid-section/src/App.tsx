5b631e7903909648a50d8a5b31a263f9a65aacef
Ncticn <nctiicn@gmail.com>
2026-01-18 11:48:14 +0300

    The “CardTestimonial” component has been called and used within the “App”.

--- 27-testimonials-grid-section/src/App.tsx
+++ 27-testimonials-grid-section/src/App.tsx
@@ -1,8 +1,22 @@
+import CardTestimonial from "./components/CardTestimonial";
+import testimonials from "./data/ClientTestimonials.json";
 function App() {
   return (
     <>
       <main>
-        <h1>Hello World !</h1>
+        <section aria-label="Section Testimonials Grid">
+          <div className="mx-auto max-w-76.25 md:max-w-161.75 lg:max-w-278.5">
+            <div className="grid-wrapper">
+              {testimonials.map((testimonial) => (
+                <CardTestimonial
+                  {...testimonial}
+                  clientQueue={testimonial.id}
+                  key={testimonial.id}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
