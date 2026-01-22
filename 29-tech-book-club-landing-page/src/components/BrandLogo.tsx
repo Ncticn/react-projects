d345756511bdff5f2251f7677dc1215ea3987438
Ncticn <nctiicn@gmail.com>
2026-01-22 16:07:27 +0300

    The BrandLogo component has been created and completed.

---
+++ 29-tech-book-club-landing-page/src/components/BrandLogo.tsx
@@ -0,0 +1,12 @@
+export default function BrandLogo() {
+  return (
+    <a
+      href="/"
+      target="_self"
+      aria-label="Go to Homepage"
+      className="inline-block"
+    >
+      <img src="/logo.svg" alt="Company Logo" />
+    </a>
+  );
+}
