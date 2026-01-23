14670cddf860b9281dd5f779fc717a39621a9d08
Ncticn <nctiicn@gmail.com>
2026-01-23 15:09:36 +0300

    The “LayoutBase” component has been added to the “App”.

--- 29-tech-book-club-landing-page/src/App.tsx
+++ 29-tech-book-club-landing-page/src/App.tsx
@@ -1,13 +1,10 @@
+import LayoutBase from "./layout/LayoutBase";
+
 function App() {
   return (
-    <main>
-      <h1>Lorem, ipsum.</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-        incidunt ipsa quasi deserunt nobis quaerat ea laboriosam optio porro
-        minima quo, modi ab, quidem quae! Ipsa aliquam quisquam suscipit eos.
-      </p>
-    </main>
+    <>
+      <LayoutBase />
+    </>
   );
 }
 
