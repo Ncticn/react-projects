b79fe9414616e0f5a19c97474cfc0eb6ebf85606
Ncticn <nctiicn@gmail.com>
2026-01-23 17:25:36 +0300

    The “FooterBase” component has been added to the “App”.

--- 29-tech-book-club-landing-page/src/components/FooterBase.tsx
+++ 29-tech-book-club-landing-page/src/components/FooterBase.tsx
@@ -6,7 +6,7 @@ import TestimonialTotal from "./TestimonialTotal";
 function FooterBase() {
   return (
     <footer>
-      <div className="w-full rounded-t-[20px] bg-neutral-900 px-4 pt-12 pb-0 md:px-8 md:pt-16 lg:px-33.75 lg:pt-20">
+      <div className="w-full rounded-t-[20px] bg-neutral-900 bg-[url(src/assets/patterns/pattern-dark-bg.svg)] bg-cover px-4 pt-12 pb-0 md:px-8 md:pt-16 lg:px-33.75 lg:pt-20">
         <div className="flex flex-col justify-center gap-16">
           <div className="footer-content mx-auto space-y-10 md:max-w-full lg:max-w-188.25">
             <p className="text-preset-2 text-center text-neutral-100">
