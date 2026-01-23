8a03045198b83a87548ae6e35ce285e59408549a
Ncticn <nctiicn@gmail.com>
2026-01-23 15:04:26 +0300

    The “theme” prop has been added to the TestimonialTotal component.

--- 29-tech-book-club-landing-page/src/components/TestimonialTotal.tsx
+++ 29-tech-book-club-landing-page/src/components/TestimonialTotal.tsx
@@ -1,4 +1,8 @@
-function TestimonialTotal() {
+interface TestimonialTotalProps {
+  theme: "dark" | "light";
+}
+
+function TestimonialTotal({ theme }: TestimonialTotalProps) {
   return (
     <div className="testimonial-total inline-block">
       <div className="flew-row flex items-center justify-start gap-3">
@@ -10,7 +14,9 @@ function TestimonialTotal() {
         </div>
         <div className="testimonial-rating flex flex-col items-start justify-start gap-1">
           <div className="icon-wrapper h-6 w-30 bg-[url(src/assets/icons/icon-star.svg)] bg-contain bg-left"></div>
-          <span className="text-preset-7 text-neutral-700">
+          <span
+            className={`text-preset-7 ${theme === "dark" ? "text-white" : "text-neutral-700"}`}
+          >
             200+ developers joined already
           </span>
         </div>
