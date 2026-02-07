61dcb759d6abe2fd09d97de72a4d47056b992159
Ncticn <nctiicn@gmail.com>
2026-02-07 10:09:02 +0300

    The homepage has been created and content has been added.

--- 31-news-homepage/src/layout/LayoutBase.tsx
+++ 31-news-homepage/src/layout/LayoutBase.tsx
@@ -1,7 +1,7 @@
 import CardArticle from "../components/CardArticle";
 import ArticleList from "../components/ArticleList";
-import ButtonBase from "../components/ButtonBase";
 import HeaderBase from "../components/HeaderBase";
+import Homepage from "../pages/Homepage";
 
 const articleItems = [
   {
@@ -29,38 +29,7 @@ export default function LayoutBase() {
     <>
       <HeaderBase />
       <main>
-        <section aria-label="Section Hero">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 lg:grid-cols-2 lg:grid-rows-1">
-            <picture className="col-span-1 lg:col-span-2">
-              <source
-                srcSet="/images/image-web-3-desktop.jpg"
-                media="(width >= 768px)"
-              />
-              <source
-                srcSet="/images/image-web-3-mobile.jpg"
-                media="(width >= 0px)"
-              />
-              <img
-                src="/images/image-web-3-desktop.jpg"
-                alt="Image Lego's"
-                className="object-contain"
-              />
-            </picture>
-            <h1 className="text-preset-1 text-navy-950 col-span-1">
-              The Bright Future of Web 3.0?
-            </h1>
-            <div className="col-span-1">
-              <div className="flex flex-col items-start justify-between gap-8">
-                <p className="text-preset-6 text-navy-600">
-                  We dive into the next evolution of the web that claims to put
-                  the power of the platforms back into the hands of the people.
-                  But is it really fulfilling its promise?
-                </p>
-                <ButtonBase />
-              </div>
-            </div>
-          </div>
-        </section>
+        <Homepage />
       </main>
       <aside>
         <ArticleList />
