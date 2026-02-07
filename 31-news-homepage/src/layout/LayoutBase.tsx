5d1de38cc24f47bd514ace9003f2848b73c33b80
Ncticn <nctiicn@gmail.com>
2026-02-07 09:28:56 +0300

    The LayoutBase Component is complete and ready for use.

---
+++ 31-news-homepage/src/layout/LayoutBase.tsx
@@ -0,0 +1,83 @@
+import CardArticle from "../components/CardArticle";
+import ArticleList from "../components/ArticleList";
+import ButtonBase from "../components/ButtonBase";
+import HeaderBase from "../components/HeaderBase";
+
+const articleItems = [
+  {
+    id: "1",
+    title: "Reviving Retro PCs",
+    description: "What happens when old PCs are given modern upgrades?",
+    imgUrl: "/images/image-retro-pcs.jpg",
+  },
+  {
+    id: "2",
+    title: "Top 10 Laptops of 2022",
+    description: "Our best picks for various needs and budgets.",
+    imgUrl: "/images/image-top-laptops.jpg",
+  },
+  {
+    id: "3",
+    title: "The Growth of Gaming",
+    description: "How the pandemic has sparked fresh opportunities.",
+    imgUrl: "/images/image-gaming-growth.jpg",
+  },
+];
+
+export default function LayoutBase() {
+  return (
+    <>
+      <HeaderBase />
+      <main>
+        <section aria-label="Section Hero">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 lg:grid-cols-2 lg:grid-rows-1">
+            <picture className="col-span-1 lg:col-span-2">
+              <source
+                srcSet="/images/image-web-3-desktop.jpg"
+                media="(width >= 768px)"
+              />
+              <source
+                srcSet="/images/image-web-3-mobile.jpg"
+                media="(width >= 0px)"
+              />
+              <img
+                src="/images/image-web-3-desktop.jpg"
+                alt="Image Lego's"
+                className="object-contain"
+              />
+            </picture>
+            <h1 className="text-preset-1 text-navy-950 col-span-1">
+              The Bright Future of Web 3.0?
+            </h1>
+            <div className="col-span-1">
+              <div className="flex flex-col items-start justify-between gap-8">
+                <p className="text-preset-6 text-navy-600">
+                  We dive into the next evolution of the web that claims to put
+                  the power of the platforms back into the hands of the people.
+                  But is it really fulfilling its promise?
+                </p>
+                <ButtonBase />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <aside>
+        <ArticleList />
+      </aside>
+      <footer>
+        <div className="flex flex-col gap-10 lg:flex-row">
+          {articleItems.map((articleItem) => (
+            <CardArticle
+              count={"0" + articleItem.id}
+              description={articleItem.description}
+              title={articleItem.title}
+              imgUrl={articleItem.imgUrl}
+              key={articleItem.id}
+            />
+          ))}
+        </div>
+      </footer>
+    </>
+  );
+}
