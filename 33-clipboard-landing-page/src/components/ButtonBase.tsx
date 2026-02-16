27e37927fc0ac7bfe7c9a1d5e0dd9d3b6a36010c
Ncticn <nctiicn@gmail.com>
2026-02-16 16:47:37 +0300

    The style class “w-full” has been added to the ButtonBase component for responsive design.

--- 33-clipboard-landing-page/src/components/ButtonBase.tsx
+++ 33-clipboard-landing-page/src/components/ButtonBase.tsx
@@ -7,7 +7,7 @@ export default function ButtonBase(props: ButtonBaseProps) {
   return (
     <button
       type="button"
-      className={`inline-flex cursor-pointer items-center justify-center rounded-[28px] px-6.25 py-3.25 inset-shadow-[0_-3px_0_0_rgba(0,0,0,0.15)] transition-all delay-75 ease-linear ${props.type === "primary" ? "bg-green-500 shadow-green-500 hover:bg-green-300 hover:shadow-green-300" : "bg-blue-500 shadow-blue-500 drop-shadow-[0_10px_20px_rgba(0,0,0,0.10)] hover:bg-blue-400 hover:shadow-blue-400"} `}
+      className={`inline-flex w-full cursor-pointer items-center justify-center rounded-[28px] px-6.25 py-3.25 inset-shadow-[0_-3px_0_0_rgba(0,0,0,0.15)] transition-all delay-75 ease-linear md:w-fit ${props.type === "primary" ? "bg-green-500 shadow-green-500 hover:bg-green-300 hover:shadow-green-300" : "bg-blue-500 shadow-blue-500 hover:bg-blue-400 hover:shadow-blue-400"} `}
     >
       <span className="text-preset-7-semibold text-white">{props.text}</span>
     </button>
