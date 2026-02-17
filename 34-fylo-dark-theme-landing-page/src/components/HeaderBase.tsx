8c8d18d1dcb979d100875cc72852d71eb7c40513
Ncticn <nctiicn@gmail.com>
2026-02-17 15:33:50 +0300

    The HeaderBase Component is complete and ready for use.

---
+++ 34-fylo-dark-theme-landing-page/src/components/HeaderBase.tsx
@@ -0,0 +1,39 @@
+export default function HeaderBase() {
+  return (
+    <header>
+      <div className="bg-navy-950">
+        <div className="mx-auto max-w-xs md:max-w-2xl lg:max-w-7xl">
+          <div className="flex flex-row items-center justify-between">
+            <a
+              href="/"
+              aria-label="Go to homepage"
+              className="max-w-20 md:max-w-35"
+            >
+              <img
+                src="/logo.svg"
+                alt="Brand Logo"
+                className="object-contain"
+              />
+            </a>
+
+            <nav>
+              <ul className="flex flex-row items-center justify-start gap-6 md:gap-12 lg:gap-14">
+                {["Features", "Team", "Sign In"].map((listItem, index) => (
+                  <li key={index}>
+                    <a
+                      href="/"
+                      aria-label={`Go to ${listItem}`}
+                      className="text-preset-3-regular block text-white transition-all delay-75 ease-linear hover:font-bold"
+                    >
+                      {listItem}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
