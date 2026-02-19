5c05709d39951e38c36a2c2b2371e055fd04d627
Ncticn <nctiicn@gmail.com>
2026-02-19 15:30:50 +0300

    ButtonText Component style colors have been updated.

--- 34-fylo-dark-theme-landing-page/src/components/ButtonText.tsx
+++ 34-fylo-dark-theme-landing-page/src/components/ButtonText.tsx
@@ -13,14 +13,14 @@ export default function ButtonText(props: ButtonTextProps) {
     <a
       href={props.url}
       aria-label={props.ariaLabel}
-      className={`group inline-flex flex-row items-center justify-center gap-2 ${props.class ? props.class : ""}`}
+      className={`group inline-flex flex-row items-center justify-center gap-2 border-b border-blue-300 pb-1 transition-all delay-75 ease-linear hover:border-white ${props.class ? props.class : ""}`}
     >
-      <span className="text-preset-7-regular text-white transition-all delay-75 ease-linear group-hover:text-blue-300">
+      <span className="text-preset-7-regular text-blue-300 transition-all delay-75 ease-linear group-hover:text-white">
         {props.text}
       </span>
       <FontAwesomeIcon
         icon={faCircleArrowRight}
-        className="text-white drop-shadow-[0_0_2px_rgba(98,224,217,81.11%)] transition-all delay-75 ease-linear group-hover:text-blue-300"
+        className="text-blue-300 drop-shadow-[0_0_2px_rgba(98,224,217,81.11%)] transition-all delay-75 ease-linear group-hover:text-white"
       />
     </a>
   );
