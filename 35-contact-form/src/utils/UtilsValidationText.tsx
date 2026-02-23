88e7efdec286a62d90093d74685b8013704b39f1
Ncticn <nctiicn@gmail.com>
2026-02-23 18:24:33 +0300

    UtilValidationText was written for the InputText component.

---
+++ 35-contact-form/src/utils/UtilsValidationText.tsx
@@ -0,0 +1,15 @@
+function UtilsValidationText(value: string) {
+  const errors: string[] = [];
+  const inputValue = value.trim();
+
+  if (inputValue.length === 0) errors.push("This field is required");
+  if (inputValue.length < 2) errors.push("It must contain at least 2 letters.");
+  if (/[0-9]/.test(inputValue)) errors.push("Can not be number");
+
+  return {
+    isValid: errors.length === 0,
+    error: errors[0],
+  };
+}
+
+export default UtilsValidationText;
