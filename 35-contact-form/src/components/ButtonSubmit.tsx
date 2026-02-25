9ce362e50abca224dac1ac765b15d1e32668bb53
Ncticn <nctiicn@gmail.com>
2026-02-25 12:32:10 +0300

    The “disabled” attribute and optional “class” attribute have been added to the ButtonSubmit component.

--- 35-contact-form/src/components/ButtonSubmit.tsx
+++ 35-contact-form/src/components/ButtonSubmit.tsx
@@ -1,12 +1,15 @@
 interface ButtonSubmit {
   text: string;
+  class?: string;
+  disabled: boolean;
 }
 
 export default function ButtonSubmit(props: ButtonSubmit) {
   return (
     <button
       type="submit"
-      className="text-body-md z-10 inline-block w-full cursor-pointer overflow-hidden rounded-lg bg-green-600 px-10 py-4 font-bold text-white transition-colors delay-75 ease-linear hover:bg-[#063F34]"
+      className={`text-body-md z-10 inline-block w-full overflow-hidden rounded-lg bg-green-600 px-10 py-4 font-bold text-white transition-all delay-75 ease-linear ${props.class ? props.class : "cursor-pointer opacity-100 hover:bg-[#063F34]"}`}
+      disabled={props.disabled}
     >
       {props.text}
     </button>
