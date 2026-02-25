906afe7a01ef6be3ce199f899aeb376d9ca10f77
Ncticn <nctiicn@gmail.com>
2026-02-25 12:34:37 +0300

    The ContactPage page was created and the FormContact component was called within it.

---
+++ 35-contact-form/src/pages/ContactPage.tsx
@@ -0,0 +1,9 @@
+import FormContact from "../components/FormContact";
+
+export default function ContactPage() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <FormContact />
+    </main>
+  );
+}
