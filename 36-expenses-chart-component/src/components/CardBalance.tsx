68be3317c14b42c191f750cbcd88774a9ea8961b
Ncticn <nctiicn@gmail.com>
2026-03-04 14:01:22 +0300

    It was updated using the BalanceInfo and ChartBar Components within the CardBalance Component.

--- 36-expenses-chart-component/src/components/CardBalance.tsx
+++ 36-expenses-chart-component/src/components/CardBalance.tsx
@@ -1,22 +1,82 @@
-interface CardBalance {
-  totalBalance: string;
-  balanceCurrency: "$" | "€";
+// DB
+import data from "../data/data.json";
+
+// Components
+import BalanceInfo from "./BalanceInfo";
+import ChartBar from "./ChartBar";
+
+const TOTAL_BALANCE = 921.48;
+const TOTAL_THIS_MONTH = 478.33;
+const PERCENT_CHANGE = 2.4;
+
+interface CardBalanceProps {
+  currency: "$" | "€" | "£";
 }
 
-export default function CardBalance(props: CardBalance) {
+export default function CardBalance({ currency = "$" }: CardBalanceProps) {
   return (
-    <div className="card-balance">
-      <div className="max-w-135 rounded-[20px] bg-red-500 px-8 py-6">
-        <div className="flex flex-row items-center justify-between">
-          <div className="space-y-2">
-            <p className="text-preset-4 font-normal text-white">My balance</p>
-            <p className="text-preset-2 text-white">
-              {props.balanceCurrency}
-              {props.totalBalance}
-            </p>
-          </div>
-          <div>
-            <img src="/logo.svg" alt="Brand Logo" className="object-contain" />
+    <article className="card-expenses-chart">
+      <div className="max-w-135 space-y-6">
+        <BalanceInfo currency={currency} totalBalance={TOTAL_BALANCE} />
+
+        <CardSpending />
+      </div>
+    </article>
+  );
+}
+
+function CardSpending() {
+  const amounts = data.map((item) => item.amount);
+  const maxValue = Math.max(...amounts);
+
+  return (
+    <div aria-label="Card Spending">
+      <div className="rounded-[20px] bg-white px-10 py-8">
+        <div className="card-wrapper flex flex-col items-stretch justify-center gap-16">
+          <h1 className="text-preset-2 text-brown-950">
+            Spending - Last 7 days
+          </h1>
+
+          <div
+            className="flex flex-col items-stretch gap-8"
+            aria-label="Chart Spending"
+          >
+            <div aria-label="Chart Bars">
+              <div className="flex flex-row items-end gap-4">
+                {data.map((item) => (
+                  <ChartBar
+                    title={item.day}
+                    value={item.amount}
+                    key={item.day}
+                    isBiggest={item.amount === maxValue}
+                  />
+                ))}
+              </div>
+            </div>
+            <div aria-label="Total Info" className="space-y-8">
+              <div className="block h-0.5 rounded-[1px] bg-red-100"></div>
+              <div aria-label="Total Details">
+                <p className="text-brown-400 text-preset-4 font-normal">
+                  Total this month
+                </p>
+                <div
+                  className="flex flex-row items-center justify-between"
+                  aria-label="Total Amount"
+                >
+                  <p className="text-preset-1 text-brown-950 w-fit text-start">
+                    ${TOTAL_THIS_MONTH}
+                  </p>
+                  <div className="w-fit text-end" aria-label="Total Change">
+                    <p className="text-preset-4 text-brown-950 font-bold">
+                      +{PERCENT_CHANGE}%
+                    </p>
+                    <p className="text-preset-4 text-brown-400 font-normal">
+                      from last month
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
