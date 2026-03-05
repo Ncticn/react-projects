b0f9c0055ea22d6f5b0416d3cd9e010116875fce
Ncticn <nctiicn@gmail.com>
2026-03-05 14:09:30 +0300

    The ButtonBase Component has been created and completed. It is now ready for use.

---
+++ 37-typemaster-pre-launch-landing-page/src/components/ButtonBase.tsx
@@ -0,0 +1,32 @@
+interface ButtonBaseProps {
+  buttonType: "primary" | "secondary";
+  buttonText: string;
+}
+
+export default function ButtonBase({
+  buttonText = "Pre-order now",
+  buttonType = "primary",
+}: ButtonBaseProps) {
+  let currentClass: string;
+
+  if (buttonType == "primary") {
+    currentClass = "bg-orange-500 hover:bg-orange-300  text-neutral-0";
+  } else {
+    currentClass =
+      "bg-neutral-200 hover:bg-neutral-900 text-neutral-900 hover:text-neutral-0";
+  }
+
+  return (
+    <a
+      href="#"
+      aria-label="#"
+      className={`inline-block h-full w-fit transition-all delay-75 ease-linear focus:rounded-lg focus:p-0.5 focus:outline-4 ${buttonType == "primary" ? "outline-orange-500" : "outline-neutral-900"}`}
+    >
+      <div
+        className={`inline-flex items-center justify-center gap-2.5 rounded-lg px-6 py-4 transition-all delay-75 ease-linear ${currentClass}`}
+      >
+        <span className="text-preset-6 block uppercase">{buttonText}</span>
+      </div>
+    </a>
+  );
+}
