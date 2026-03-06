0709119462f67de7602e228c932ad2649c9f97b8
Ncticn <nctiicn@gmail.com>
2026-03-06 15:13:01 +0300

    Screen size responsiveness has been added for the HeaderBase Component.

--- 37-typemaster-pre-launch-landing-page/src/components/HeaderBase.tsx
+++ 37-typemaster-pre-launch-landing-page/src/components/HeaderBase.tsx
@@ -4,7 +4,7 @@ import ButtonBase from "./ButtonBase";
 export default function HeaderBase() {
   return (
     <header>
-      <div className="header-body">
+      <div className="mx-auto max-w-85 md:max-w-176 lg:max-w-277.5">
         <div className="flex items-center justify-between gap-0">
           <BrandLogo />
           <ButtonBase buttonType="secondary" buttonText="Pre-order now" />
