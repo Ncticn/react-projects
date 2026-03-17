b57e8d831b0b9bc49bd02434b43c3b67e19df5cf
Ncticn <nctiicn@gmail.com>
2026-03-17 16:43:54 +0300

    The padding values have been updated.

--- 39-project-tracking-intro-component/src/components/ButtonApp.tsx
+++ 39-project-tracking-intro-component/src/components/ButtonApp.tsx
@@ -12,7 +12,7 @@ export default function ButtonApp({
   return (
     <a
       href={buttonLink}
-      className={`hover:bg-red-350 inline-flex items-center justify-center gap-2 rounded-[5px] bg-red-400 px-9 py-3.75 transition-colors delay-75 ease-linear ${buttonClass}`}
+      className={`hover:bg-red-350 inline-flex items-center justify-center gap-2 rounded-[5px] bg-red-400 px-6 py-3.5 transition-colors delay-75 ease-linear md:px-9 md:py-3.75 ${buttonClass}`}
     >
       <span className="text-preset-4 tracking-1px font-bold text-white uppercase">
         {buttonText}
