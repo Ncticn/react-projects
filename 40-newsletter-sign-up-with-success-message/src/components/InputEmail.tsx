1f537594fead3a43fb46a7a803cf618ce469c743
Ncticn <nctiicn@gmail.com>
2026-03-23 14:18:53 +0300

    The UtilValidationEmail component has been integrated into the InputEmail component and is now complete.

--- 40-newsletter-sign-up-with-success-message/src/components/InputEmail.tsx
+++ 40-newsletter-sign-up-with-success-message/src/components/InputEmail.tsx
@@ -1,33 +1,43 @@
 "use client"
+import UtilValidationEmail from "@/utils/UtilValidationEmail";
 import { useState } from "react";
 
 interface InputEmailProps {
     inputID: string;
-    inputName: string;
+    inputName?: string;
+    inputLabel: string;
+    placeholder?: string;
 }
 
-export default function InputEmail() {
+export default function InputEmail({ inputLabel = "Label", placeholder = "example@email.com", inputID, inputName = "input-user-email" }: InputEmailProps) {
     const [inputValue, setInputValue] = useState("");
     const [error, setError] = useState(false);
 
-    let inputClass = "border-grey bg-white";
+    let inputClass = "border-grey bg-white"
 
-    if (inputValue) {
-        inputClass = "border-blue-800 bg-white text-blue-800"
+    if (inputValue !== "") {
+        inputClass = "border-blue-800 bg-white text-blue-800";
         if (error) {
             inputClass = "border-red bg-red-100 text-red"
         }
     }
 
 
+    function handleOnChange(value: string) {
+        setInputValue(value);
+
+        const result = UtilValidationEmail(value);
+        setError(!result.isValid);
+    }
+
 
     return (
-        <div className="flex flex-col gap-2 items-start justify-start">
+        <div className="form-input-item flex flex-col gap-2 items-start justify-start">
             <div className="flex flex-row items-center justify-between w-full">
-                <label htmlFor="input-user-email" className="text-preset-3 text-blue-800">Email address</label>
-                <span className={`text-preset-3 text-red ${error ? "opacity-100" : "opacity-0"} transition delay-75 ease-linear`}>Valid email required</span>
+                <label htmlFor="input-user-email" className="text-preset-3 text-blue-800">{inputLabel}</label>
+                <span className={`text-preset-3 text-red ${error && inputValue ? "opacity-100" : "opacity-0"} transition delay-75 ease-linear`}>Valid email required</span>
             </div>
-            <input type="email" name="input-email" id="input-user-email" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className={`px-6 py-4 rounded-lg outline-0 border text-preset-2 font-normal invalid:border-blue-800 transition delay-75 ease-linear ${inputClass}`} placeholder="email@company.com" />
+            <input type="email" name={inputName} id={inputID} value={inputValue} onChange={(e) => handleOnChange(e.target.value)} className={`px-6 py-4 rounded-lg outline-0 border text-preset-2 font-normal transition delay-75 ease-linear ${inputClass}`} placeholder={placeholder} />
         </div>
     );
 }
