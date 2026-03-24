838d23323c83fbadebdc4c5fe600f1e3b5f09841
Ncticn <nctiicn@gmail.com>
2026-03-24 15:19:19 +0300

    The “text” and “disabled” props have been added to ButtonSubmit.

--- 40-newsletter-sign-up-with-success-message/src/components/ButtonSubmit.tsx
+++ 40-newsletter-sign-up-with-success-message/src/components/ButtonSubmit.tsx
@@ -1,7 +1,12 @@
 "use client"
 
-export default function ButtonSubmit() {
+interface ButtonSubmitProps {
+    text: string;
+    disabled?: boolean;
+}
+
+export default function ButtonSubmit({ text, disabled = false }: ButtonSubmitProps) {
     return (
-        <button type="submit" className="text-preset-2 w-full font-bold text-white bg-linear-to-r hover:from-0% hover:from-[#FF6A3A] hover:to-100% hover:to-[#FF527B] bg-blue-800 cursor-pointer rounded-lg px-8 py-4 transition delay-75 ease-linear hover:drop-shadow-[0_16px_32px_rgba(255,97,85,50%)]">Subscribe to monthly newsletter</button>
+        <button type="submit" className={`text-preset-2 w-full font-bold text-white bg-linear-to-r bg-blue-800 rounded-lg px-8 py-4 transition delay-75 ease-linear ${disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer opacity-100 hover:drop-shadow-[0_16px_32px_rgba(255,97,85,50%)] hover:from-0% hover:from-[#FF6A3A] hover:to-100% hover:to-[#FF527B]"}`} disabled={disabled}>{text}</button>
     );
 }
