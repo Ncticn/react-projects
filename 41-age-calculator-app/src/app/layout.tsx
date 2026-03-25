4e45f45a963758d9726729a66942ac08a48bdbbd
Ncticn <nctiicn@gmail.com>
2026-03-25 14:45:42 +0300

    Prettier has been added and installed.

--- 41-age-calculator-app/src/app/layout.tsx
+++ 41-age-calculator-app/src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-
-import "@/libraries/tailwind.css"
-
+import "@/libraries/tailwind.css";
 
 export const metadata: Metadata = {
-  title: "Age Calculator App",
-  description: "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!",
+  title: "Frontend Mentor | Age Calculator App",
+  description:
+    "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!",
 };
 
 export default function RootLayout({
@@ -14,14 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className="h-full antialiased"
-    >
-      <body className="min-h-full flex flex-col">
-        <main>
-          {children}
-        </main>
+    <html lang="en" className="h-full antialiased">
+      <body className="flex min-h-full flex-col">
+        <main>{children}</main>
       </body>
     </html>
   );
