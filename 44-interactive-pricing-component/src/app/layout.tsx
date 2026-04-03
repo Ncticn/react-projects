f34f3bdc82df06db549d129b5bc3146bfb379685
Ncticn <nctiicn@gmail.com>
2026-04-03 15:36:30 +0300

    Prettier has been added and installed.

--- 44-interactive-pricing-component/src/app/layout.tsx
+++ 44-interactive-pricing-component/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Interactive Pricing Component",
-  description: "In this project, you'll build out an interactive pricing component complete with custom range input slider and pricing toggle. A perfect way to test your JS skills!",
+  description:
+    "In this project, you'll build out an interactive pricing component complete with custom range input slider and pricing toggle. A perfect way to test your JS skills!",
 };
 
 export default function RootLayout({
@@ -11,11 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className="h-full antialiased"
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased">
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
--- 44-interactive-pricing-component/src/app/page.tsx
+++ 44-interactive-pricing-component/src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    
     <section>
       <h1>Lorem, ipsum.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, provident.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+        provident.
+      </p>
     </section>
   );
 }
