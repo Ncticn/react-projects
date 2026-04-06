c662cfd87bb7dd24c40b1a0432fb035199b20632
Ncticn <nctiicn@gmail.com>
2026-04-06 16:38:20 +0300

    The Tag component has been updated for Responsive.

--- 44-interactive-pricing-component/src/components/Tag/index.tsx
+++ 44-interactive-pricing-component/src/components/Tag/index.tsx
@@ -5,7 +5,10 @@ import styles from "@/components/Tag/tag.module.css";
 function Tag({ text }: { text: string }) {
   return (
     <div className={styles.tag}>
-      <span className={styles.tag_text}>{text}</span>
+      <span className={`${styles.tag_text} hidden md:block`}>
+        {text} discount
+      </span>
+      <span className={`${styles.tag_text} md:hidden`}>-{text}</span>
     </div>
   );
 }
