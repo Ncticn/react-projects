b1bcc92866a40e2854448a8e1d85978865e1774d
Ncticn <nctiicn@gmail.com>
2026-04-06 18:32:21 +0300

    Prettier has been added and installed.

--- 45-maker-pre-launch-landing-page/src/app/layout.tsx
+++ 45-maker-pre-launch-landing-page/src/app/layout.tsx
@@ -6,12 +6,13 @@ import { Manrope } from "next/font/google";
 
 const manropeSans = Manrope({
   variable: "--font-manrope-sans",
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Maker Pre-Launch Landing Page",
-  description: "Put your layout skills to the test with this landing page challenge.",
+  description:
+    "Put your layout skills to the test with this landing page challenge.",
 };
 
 export default function RootLayout({
@@ -20,11 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${manropeSans.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${manropeSans.variable} h-full antialiased`}>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
--- 45-maker-pre-launch-landing-page/src/app/page.tsx
+++ 45-maker-pre-launch-landing-page/src/app/page.tsx
@@ -2,7 +2,11 @@ export default function Home() {
   return (
     <section>
       <h1>Lorem, ipsum.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga enim ea a inventore? Est possimus doloribus iste quis, maxime, impedit nostrum cumque odio amet aspernatur autem animi minus in.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga
+        enim ea a inventore? Est possimus doloribus iste quis, maxime, impedit
+        nostrum cumque odio amet aspernatur autem animi minus in.
+      </p>
     </section>
   );
 }
