92d2690d0ebcab9be151361dc86fff6235c65a26
Ncticn <nctiicn@gmail.com>
2026-04-08 17:28:46 +0300

    Custom TailwindCSS settings have been applied. The font has been loaded and classes have been added.

--- 46-coding-bootcamp-testimonials-slider/src/app/layout.tsx
+++ 46-coding-bootcamp-testimonials-slider/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import "@/libraries/tailwind.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Coding Bootcamp Testimonials Slider",
   description: "This challenge will be a nice test if you're new to JavaScript. It's also a great opportunity to play around with content animations and transitions.",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={`${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
