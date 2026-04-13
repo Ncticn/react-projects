f0bc7b2880924ee2f8dfdf483e998de37ef62c1a
Ncticn <nctiicn@gmail.com>
2026-04-13 20:06:49 +0300

    The “SelectedSocialMedia” utility function has been created and completed.

---
+++ 47-social-media-dashboard-with-theme-switcher/src/utils/SelectedSocialMedia.ts
@@ -0,0 +1,25 @@
+import IconFacebook from "@/src/assets/icons/icon-facebook.svg";
+import IconTwitter from "@/src/assets/icons/icon-twitter.svg";
+import IconInstagram from "@/src/assets/icons/icon-instagram.svg";
+import IconYoutube from "@/src/assets/icons/icon-youtube.svg";
+
+export default function SelectedSocialMedia( socialMedia: "facebook" | "twitter" | "instagram" | "youtube" ) {
+    let selectedMedia;
+
+    switch (socialMedia) {
+        case "facebook":
+            selectedMedia = IconFacebook;
+            break;
+        case "twitter":
+            selectedMedia = IconTwitter;
+            break;
+        case "instagram":
+            selectedMedia = IconInstagram;
+            break;
+        case "youtube":
+            selectedMedia = IconYoutube;
+            break;
+    }
+
+    return selectedMedia;
+}
