f43f17258003ec51d93987abbb9be161265475c8
Ncticn <nctiicn@gmail.com>
2026-04-14 13:22:08 +0300

    A comparison type was added to the component, and it was reorganized accordingly.

--- 47-social-media-dashboard-with-theme-switcher/src/components/ui/CardValueComparison/index.tsx
+++ 47-social-media-dashboard-with-theme-switcher/src/components/ui/CardValueComparison/index.tsx
@@ -10,10 +10,20 @@ import IconDown from "@/src/assets/icons/icon-down.svg";
 // Utility Function
 import PercentageDifference from "@/src/utils/PercentageDifference ";
 
+// Props
+interface CardValueComparisonProps {
+    description: string,
+    type: "percent" | "difference"
+    newValue: number,
+    oldValue: number,
+}
 
-function CardValueComparison({ newValue, oldValue, description }: { newValue: number, oldValue: number, description:string }) {
+function CardValueComparison({ newValue, oldValue, description, type }: CardValueComparisonProps) {
+    let result: number = newValue - oldValue;;
 
-    const result = PercentageDifference(newValue, oldValue);
+    if (type === "percent") {
+        result = PercentageDifference(newValue, oldValue);
+    }
 
     return (
         <div className="flex flex-row items-center justify-center gap-2">
@@ -22,7 +32,8 @@ function CardValueComparison({ newValue, oldValue, description }: { newValue: nu
                     ? <Image src={IconUp} alt="Icon Up" />
                     : <Image src={IconDown} alt="Icon Down" />
             }
-            <span className={`text-preset-6 ${result > 0 ? "text-green-500" : "text-red-500"} `}>{result.toString().replace("-", "")}
+            <span className={`text-preset-6 ${result > 0 ? "text-green-500" : "text-red-500"} `}>
+                {result.toString().replace("-", "")}
                 {description}
             </span>
         </div>
