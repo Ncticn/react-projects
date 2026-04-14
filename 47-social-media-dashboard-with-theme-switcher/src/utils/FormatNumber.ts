174a229c81ab988523178fddd2d61e5f060390c4
Ncticn <nctiicn@gmail.com>
2026-04-14 16:29:28 +0300

    FormatData.json has been created and completed.

---
+++ 47-social-media-dashboard-with-theme-switcher/src/utils/FormatNumber.ts
@@ -0,0 +1,12 @@
+export default function FormatNumber(value:number) {
+    const stringValue = value.toString();
+    if(stringValue.length > 4 && stringValue.length < 7){
+        if(stringValue.length == 6 ){
+            return stringValue.slice(0,3) + "K";
+        }else if(stringValue.length == 5){
+            return stringValue.slice(0 ,2) + "K";
+        }
+    }else{
+        return value;
+    }
+}
