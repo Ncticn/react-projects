71c93a72060d52ede97b764b172f16b666f3edd3
Ncticn <nctiicn@gmail.com>
2026-04-15 16:13:29 +0300

    The Divider component has been adapted to the design.

---
+++ 48-pricing-component-with-toggle/src/components/ui/Divider/index.tsx
@@ -0,0 +1,9 @@
+
+// Component Styles
+import styles from "@/components/ui/Divider/Divider.module.css"
+
+export default function Divider(){
+    return(
+        <div className={styles.divider}></div>
+    );
+}
