fbe25fe22ae0a0258081f4f8752361386fdc8940
Ncticn <nctiicn@gmail.com>
2026-04-15 18:01:35 +0300

    Divider styles have been added as classes.

--- 48-pricing-component-with-toggle/src/components/ui/Divider/index.tsx
+++ 48-pricing-component-with-toggle/src/components/ui/Divider/index.tsx
@@ -1,9 +1,6 @@
 
-// Component Styles
-import styles from "@/components/ui/Divider/Divider.module.css"
-
 export default function Divider(){
     return(
-        <div className={styles.divider}></div>
+        <div className="w-full h-px bg-gray-650 opacity-25 group-[.highlighted]:bg-white"></div>
     );
 }
