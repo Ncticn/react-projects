f1e65929235254ca91bbbc67a6d944c8bbaf6b4f
Ncticn <nctiicn@gmail.com>
2026-04-16 17:06:06 +0300

    The layout has been completed.

--- 48-pricing-component-with-toggle/src/app/layout.tsx
+++ 48-pricing-component-with-toggle/src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google"
 import "@/libraries/tailwind.css";
 
+// NextJS
+import Image from "next/image";
+
+// Images
+import BgTop from "@/assets/images/bg-top.svg";
+import BgBottom from "@/assets/images/bg-bottom.svg";
+
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Pricing Component with Toggle",
@@ -23,7 +30,13 @@ export default function RootLayout({
       lang="en"
       className={`${montserrat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="relative overflow-x-hidden min-h-full flex flex-col items-stretch justify-center">
+        <main>
+          {children}
+        </main>
+        <Image src={BgTop} alt="" className="object-center object-cover absolute right-0 top-0 z-10" loading="eager" />
+        <Image src={BgBottom} alt="" className="object-center object-cover absolute left-0 bottom-0 z-10 hidden lg:inline-block" loading="eager" />
+      </body>
     </html>
   );
 }
