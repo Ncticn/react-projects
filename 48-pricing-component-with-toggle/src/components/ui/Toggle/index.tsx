3453a4fe4f503a242fda6d1dd6f5fb99fe8be288
Ncticn <nctiicn@gmail.com>
2026-04-16 17:04:57 +0300

    The Toggle component has been completed.

--- 48-pricing-component-with-toggle/src/components/ui/Toggle/index.tsx
+++ 48-pricing-component-with-toggle/src/components/ui/Toggle/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 // React
 import { useState } from "react";
 
@@ -10,10 +9,19 @@ import { motion } from "motion/react"
 // Component Styles
 import styles from "@/components/ui/Toggle/Toggle.module.css";
 
+interface ToggleProps{
+    currentlySelected : (currently: string) => void;
+}
 
-export default function Toggle() {
-    const [isOn, setIsOn] = useState<boolean>(false);
-    const toggleSwitch = () => setIsOn((prev) => !prev);
+export default function Toggle({currentlySelected}:ToggleProps) {
+    const [isOn, setIsOn] = useState<boolean>(true);
+    
+    function toggleSwitch(){
+        setIsOn(!isOn);
+        
+        if(isOn) currentlySelected("annually");
+        else currentlySelected("monthly");
+    }
 
     return (
         <div className={styles.toggleContainer}>
