2e86a7cdadf645ca9b8c1e5bf70259cabbcd1e3e
Ncticn <nctiicn@gmail.com>
2026-04-17 20:23:02 +0300

    The Header component has been created and completed. The ButtonTheme component has been redesigned.

---
+++ 49-character-counter/src/components/layout/Header/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import ButtonTheme from "@/components/ui/ButtonTheme";
+import LogoDark from "../LogoDark";
+import LogoLight from "../LogoLight";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+    const [theme, setTheme] = useState<"dark" | "light">("light");
+
+    const root = window.document.documentElement.classList;
+
+    function themeSwitch() {
+        if (theme === "light") {
+            root.add("dark");
+            localStorage.setItem("theme", "dark");
+            setTheme("dark");
+        } else {
+            root.remove("dark");
+            localStorage.setItem("theme", "light");
+            setTheme("light");
+        }
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("theme") === "dark") {
+            setTheme("dark");
+            root.add("dark");
+        }else {
+            localStorage.setItem("theme", "light");
+            setTheme("light");
+        }
+    }, []);
+
+
+    return (
+        <header>
+            <div className="flex flex-row items-center justify-between gap-0">
+                {
+                    theme === "dark" ? <LogoDark /> : <LogoLight />
+                }
+                <ButtonTheme activeTheme={themeSwitch} theme={theme} />
+            </div>
+        </header>
+    );
+}
--- 49-character-counter/src/components/ui/ButtonTheme.tsx
+++ 49-character-counter/src/components/ui/ButtonTheme.tsx
@@ -1,6 +1,4 @@
 "use client";
-// React
-import { useEffect, useState } from "react";
 
 // NextJS
 import Image from "next/image";
@@ -9,41 +7,20 @@ import Image from "next/image";
 import IconSun from "@/assets/icons/icon-sun.svg";
 import IconMoon from "@/assets/icons/icon-moon.svg";
 
-export default function ButtonTheme() {
-    const [isDark, setIsDark] = useState<boolean>(false);
+interface ButtonThemeProps {
+    activeTheme?: () => void;
+    theme: string;
+}
 
-    const root = window.document.documentElement;
-
-    const handleClick = () => {
-        if (!isDark) {
-            root.classList.add("dark");
-            localStorage.setItem("theme", "dark");
-        } else {
-            root.classList.remove("dark");
-            localStorage.setItem("theme", "light");
-        }
-        setIsDark(prev => !prev);
-    }
-
-    useEffect(() => {
-        const theme = localStorage.getItem("theme");
-
-        if (theme === "dark") {
-            setIsDark(true);
-            root.classList.add("dark");
-        }
-
-    }, []);
-
-
+export default function ButtonTheme({ activeTheme, theme }: ButtonThemeProps) {
     return (
         <button
             type="button"
             className="dark:bg-neutral-700 bg-neutral-100 rounded-lg size-11 cursor-pointer flex items-center justify-center gap-0"
-            onClick={handleClick}
+            onClick={activeTheme}
         >
             {
-                isDark ? <Image src={IconSun} alt="Icon Sun" loading="eager" /> : <Image src={IconMoon} alt="Icon Sun" loading="eager" />
+                theme === "dark" ? <Image src={IconSun} alt="Icon Sun" loading="eager" /> : <Image src={IconMoon} alt="Icon Sun" loading="eager" />
             }
         </button>
     );
