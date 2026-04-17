6540cf7378a81b70e1bbe7f1eebdeab11b1fbfef
Ncticn <nctiicn@gmail.com>
2026-04-17 20:41:42 +0300

    The InputTextarea component has been moved to the “ui” folder and its design has been improved.

--- 49-character-counter/src/components/ui/InputTextarea.tsx
+++ 49-character-counter/src/components/ui/InputTextarea.tsx
@@ -25,14 +25,14 @@ export default function InputTextarea() {
         <div className="form-input-item flex flex-col items-start justify-start gap-3">
             <textarea
                 placeholder="Start typing here… (or paste your text)"
-                className={`bg-neutral-800 resize-none border-neutral-700 drop-shadow-[0px_2px_5px_rgba(15,15,15,4%)] min-h-50 w-full rounded-xl px-5 py-5 border-2  text-preset-3 text-neutral-200 hover:bg-neutral-700 hover:border-neutral-600 outline-0 ${err ? "focus:border-orange-500 focus:drop-shadow-[0px_0px_8px_#FE8159] border-orange-500 drop-shadow-[0px_0px_8px_#FE8159]" : "focus:border-purple-500 focus:drop-shadow-[0px_0px_10px_#D3A0FA]"}`}
+                className={`w-full h-50 px-5 py-5 text-preset-3 rounded-xl outline-0 border-2 text-neutral-700 border-neutral-200 focus:drop-shadow-[0_0_10px_#D3A0FA] focus:border-purple-500 bg-neutral-100 hover:bg-neutral-200 focus:bg-neutral-200 focus:placeholder:text-neutral-900 placeholder:text-neutral-700 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-800 dark:placeholder:text-neutral-200 dark:border-neutral-700 dark:text-neutral-200 dark:hover:border-neutral-600 dark:focus:border-purple-500 dark:focus:placeholder:text-neutral-200 ${err && "border-orange-800! focus:border-orange-800! drop-shadow-[0_0_8px_#DA3701] dark:drop-shadow-[0_0_8px_#FE8159] dark:border-orange-500! dark:focus:border-orange-500!"}`}
                 value={value}
                 onChange={(e) => handleChange(e.target.value)}
             ></textarea>
             {err &&
                 <div className="flex flex-row items-center justify-start gap-2">
                     <Image src={IconInfo} alt="Icon Info" />
-                    <span className="text-preset-4 text-orange-500">Limit reached! Your text exceeds 300 characters.</span>
+                    <span className="text-preset-4 text-orange-800 dark:text-orange-500">Limit reached! Your text exceeds 300 characters.</span>
                 </div>
             }
         </div>
