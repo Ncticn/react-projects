383aa2851cf97ae4b96687f0815fb67e028eace7
Ncticn <nctiicn@gmail.com>
2026-04-22 13:50:20 +0300

    The “app” and “layout” components of the project have been completed.

--- 49-character-counter/src/app/layout.tsx
+++ 49-character-counter/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { DM_Sans } from "next/font/google";
 import type { Metadata } from "next";
 import "@/libraries/tailwind.css";
+import Header from "@/components/layout/Header";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Character Counter",
@@ -23,7 +24,12 @@ export default function RootLayout({
       lang="en"
       className={`${dmSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col items-stretch gap-10 lg:gap-12 justify-start max-w-85.75 md:max-w-176 lg:max-w-247.5 mx-auto pb-8 md:pb-23.25 pt-4 lg:pt-8 lg:pb-16">
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- 49-character-counter/src/app/page.tsx
+++ 49-character-counter/src/app/page.tsx
@@ -1,8 +1,50 @@
+"use client";
+
+// React
+import { useEffect, useState } from "react";
+
+// Components
+import TextAnalyzer from "@/components/ui/TextAnalyzer";
+import Stats from "@/components/ui/Stats";
+import LetterDensity from "@/components/ui/LetterDensity";
+
+// Utility Functions
+import utilCharactersCounter from "@/utils/utilCharactersCounter";
+import utilWordCounter from "@/utils/utilWordCounter";
+import utilSentenceCounter from "@/utils/utilSentenceCounter";
+
 
 export default function Home() {
+  const [totalCharacters, setTotalCharacters] = useState("00");
+  const [wordCount, setWordCount] = useState("00");
+  const [sentenceCount, setSentenceCount] = useState("00");
+  const [userValue, setUserValue] = useState("");
+  const [excludeSpace, setExcludeSpace] = useState<boolean>(false);
+  const [maxLength, setMaxLength] = useState<number>();
+
+  function handleTextarea(value: string, isIncluding: boolean, length?: number) {
+    setUserValue(value);
+    setExcludeSpace(isIncluding);
+    setMaxLength(length);
+  }
+
+  useEffect(() => {
+    setTotalCharacters(userValue === "" ? "00" : utilCharactersCounter(userValue, excludeSpace));
+    setWordCount(userValue === "" ? "00" : utilWordCounter(userValue));
+    setSentenceCount(userValue === "" ? "00" : utilSentenceCounter(userValue));
+
+  }, [userValue, excludeSpace, maxLength]);
+
   return (
     <section>
-      <h1>Lorem, ipsum.</h1>
+      <div className="space-y-10 lg:space-y-12">
+        <h1 className="text-preset-1 text-neutral-900 dark:text-neutral-100 text-center max-w-137.5 mx-auto">Analyze your text in real-time.</h1>
+        <TextAnalyzer text={handleTextarea} />
+        <div className="space-y-6">
+          <Stats totalCharactersValue={totalCharacters} wordCountValue={wordCount} sentenceCountValue={sentenceCount} />
+          <LetterDensity text={maxLength ? userValue.slice(0, maxLength) : userValue} />
+        </div>
+      </div>
     </section>
   );
 }
