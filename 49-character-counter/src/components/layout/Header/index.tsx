661391cb4df1a62dbe4f44239b9fb08958c85110
Ncticn <nctiicn@gmail.com>
2026-04-22 14:32:34 +0300

    The “document” object has been removed from the local function.

--- 49-character-counter/src/components/layout/Header/index.tsx
+++ 49-character-counter/src/components/layout/Header/index.tsx
@@ -11,24 +11,29 @@ import ButtonTheme from "@/components/ui/ButtonTheme";
 export default function Header() {
     const [theme, setTheme] = useState<"dark" | "light">("light");
 
-    const root = window.document.documentElement.classList;
 
     function themeSwitch() {
         if (theme === "light") {
-            root.add("dark");
             localStorage.setItem("theme", "dark");
             setTheme("dark");
         } else {
-            root.remove("dark");
             localStorage.setItem("theme", "light");
             setTheme("light");
         }
     }
 
     useEffect(() => {
+        if (theme === "light") {
+            document.documentElement.classList.remove("dark");
+        } else {
+            document.documentElement.classList.add("dark");
+        }
+    }, [theme]);
+
+    useEffect(() => {
         if (localStorage.getItem("theme") === "dark") {
             setTheme("dark");
-            root.add("dark");
+            document.documentElement.classList.add("dark");
         } else {
             localStorage.setItem("theme", "light");
             setTheme("light");
