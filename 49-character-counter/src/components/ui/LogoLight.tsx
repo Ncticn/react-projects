d3609da7a0cfb662c686f67cafe1ba88c3cefe6a
Ncticn <nctiicn@gmail.com>
2026-04-22 13:41:18 +0300

    “loading=”eager“” has been added for logos.

--- 49-character-counter/src/components/ui/LogoLight.tsx
+++ 49-character-counter/src/components/ui/LogoLight.tsx
@@ -8,7 +8,7 @@ import LogoLightTheme from "../../../public/logo-light-theme.svg";
 export default function LogoLight() {
     return (
         <Link href={"#"} aria-label="Go to homepage">
-            <Image src={LogoLightTheme} alt="Brand Logo" className="max-w-48 md:max-w-full" />
+            <Image src={LogoLightTheme} alt="Brand Logo" className="max-w-48 md:max-w-full" loading="eager" />
         </Link>
     );
 }
