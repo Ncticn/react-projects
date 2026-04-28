5e25202db151301bf9f4c3bb462866d4b8d7670a
Ncticn <nctiicn@gmail.com>
2026-04-28 19:31:09 +0300

    Props have been added for the CardResult component.

--- 50-mortgage-repayment-calculator/src/components/ui/CardResult.tsx
+++ 50-mortgage-repayment-calculator/src/components/ui/CardResult.tsx
@@ -1,16 +1,23 @@
-export default function CardResult() {
+interface CardResultProps {
+    monthlyRepayments: string;
+    totalRepay?: string;
+}
+export default function CardResult({ monthlyRepayments, totalRepay }: CardResultProps) {
+    
     return (
         <div className="px-8 py-8 border-t-4 border-lime bg-black/25 rounded-lg">
             <div className="flex flex-col items-start justify-start gap-8">
                 <div className="space-y-2">
                     <p className="text-preset-4 text-slate-300">Your monthly repayments</p>
-                    <p className="text-preset-1 text-lime">£1,797.74</p>
+                    <p className="text-preset-1 text-lime">£{monthlyRepayments}</p>
                 </div>
                 <Divider />
-                <div className="space-y-2">
-                    <p className="text-preset-4 text-slate-300">Total you'll repay over the term</p>
-                    <p className="text-preset-2 text-white">£539,322.94</p>
-                </div>
+                {totalRepay &&
+                    <div className="space-y-2">
+                        <p className="text-preset-4 text-slate-300">Total you'll repay over the term</p>
+                        <p className="text-preset-2 text-white">£{totalRepay}</p>
+                    </div>
+                }
             </div>
         </div>
     );
