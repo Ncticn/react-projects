30c38fe2859eb3981c5a82179c072cf08331be34
Ncticn <nctiicn@gmail.com>
2026-04-28 19:30:01 +0300

    The ButtonClear component has been reorganized.

--- 50-mortgage-repayment-calculator/src/components/ui/ButtonClear.tsx
+++ 50-mortgage-repayment-calculator/src/components/ui/ButtonClear.tsx
@@ -3,12 +3,14 @@
 // Props
 interface ButtonClearProps {
     label?: string;
+    onClick?: () => void;
 }
-export default function ButtonClear({ label= "Button" }: ButtonClearProps) {
+export default function ButtonClear({ label = "Button", onClick }: ButtonClearProps) {
     return (
         <button
             type="reset"
             className="text-preset-4 cursor-pointer text-slate-700 underline underline-offset-4 transition delay-150 ease-linear hover:text-slate-900"
+            onClick={onClick}
         >
             {label}
         </button>
