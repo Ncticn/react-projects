efda3b7d7091a0f2934cea883acf1806dc808211
Ncticn <nctiicn@gmail.com>
2026-04-28 19:30:39 +0300

    The InputText component has been redesigned due to a design flaw.

--- 50-mortgage-repayment-calculator/src/components/ui/InputText.tsx
+++ 50-mortgage-repayment-calculator/src/components/ui/InputText.tsx
@@ -9,7 +9,6 @@ interface InputTextProps {
     addon: string;
     addonPosition: "prefix" | "suffix";
     error?: string;
-
 }
 
 export default function InputText({
@@ -28,10 +27,10 @@ export default function InputText({
     }
 
     return (
-        <div className="flex flex-col items-stretch justify-center gap-3">
+        <div className="flex flex-col items-stretch justify-center lg:justify-start gap-3">
 
             <label htmlFor={id} className="text-preset-4 text-slate-700">{label}</label>
-            <div className={`flex items-stretch *:h-full justify-start gap-0 border-2 group focus-within:border-lime transition delay-150 ease-linear hover:border-slate-900 rounded-sm overflow-hidden relative ${error ? "border-red" : "border-slate-500"} ${addonPosition === "prefix" ? "flex-row" : "flex-row-reverse"}`}>
+            <div className={`flex items-stretch h-fit *:h-full justify-start gap-0 border-2 group focus-within:border-lime transition delay-150 ease-linear hover:border-slate-900 rounded-sm overflow-hidden relative ${error ? "border-red" : "border-slate-500"} ${addonPosition === "prefix" ? "flex-row" : "flex-row-reverse"}`}>
 
                 <div
                     className={`px-4 py-3 text-center cursor-pointer text-preset-3 transition delay-150 ease-linear group-focus-within:bg-lime group-focus-within:text-slate-900 ${error ? "bg-red text-white" : "bg-slate-100 text-slate-700"}`}
