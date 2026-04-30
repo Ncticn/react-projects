404fcca785b55776f8c580158ff430da08508e90
Ncticn <nctiicn@gmail.com>
2026-04-30 17:48:04 +0300

    The Header component was created and styled to match the design.

---
+++ 51-bmi-calculator/src/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+import Logo from "@/public/logo.svg";
+
+// NextJS
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+    return (
+        <header>
+            <div className="w-full flex flex-row items-center justify-center lg:justify-start gap-0">
+                <Link href={"#"}>
+                    <Image src={Logo} alt="Brand Logo" loading="eager" className="size-10 lg:size-16" />
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
