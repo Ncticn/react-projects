21f1f868dd46749dc83a3038882b279b05707d80
Ncticn <nctiicn@gmail.com>
2026-05-02 17:40:20 +0300

    The BarInformation component has been fully customized to match the design.

--- 51-bmi-calculator/src/components/ui/BarInformation.tsx
+++ 51-bmi-calculator/src/components/ui/BarInformation.tsx
@@ -7,17 +7,17 @@ interface BarInformationProps {
 
 export default function BarInformation({ bmi, children }: BarInformationProps) {
     return (
-        <div className="bg-blue-500 px-8 py-8 rounded-l-2xl rounded-r-2xl md:rounded-r-full w-full h-full ">
+        <div className="bg-blue-500 px-8 py-8 rounded-l-2xl rounded-r-2xl md:rounded-r-[100px] w-full h-full overflow-hidden relative">
             {
                 bmi && children ?
-                    <div className="flex flex-col md:flex-row items-start justify-center md:items-center md:justify-between gap-6 md:gap-22">
+                    <div className="flex flex-col md:flex-row items-start justify-center md:items-center md:justify-between gap-6 md:gap-0">
                         <div className="flex flex-col items-start justify-center gap-2">
                             <h6 className="text-preset-6 font-semibold text-white">
                                 Your BMI is...
                             </h6>
                             <p className="text-preset-1 text-white">23.4</p>
                         </div>
-                        <p className="text-preset-7 font-normal text-white text-start text-pretty lg:w-1/4">
+                        <p className="text-preset-7 font-normal text-white text-start text-pretty md:max-w-1/2">
                             {children}
                         </p>
                     </div>
