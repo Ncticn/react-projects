580d5939c3b60610751e7906fa81a34b897d74e8
Ncticn <nctiicn@gmail.com>
2026-05-04 15:31:07 +0300

    The InputNumber component was removed, and an InputText component was created and completed in its place.

--- 51-bmi-calculator/src/components/ui/InputText.tsx
+++ 51-bmi-calculator/src/components/ui/InputText.tsx
@@ -1,17 +1,23 @@
-interface InputNumberProps {
+"use client";
+
+// React
+import { ChangeEvent } from "react";
+
+// Props
+interface InputTextProps {
     id: string;
     name: string;
     label?: string;
     suffix: string;
     value: string;
     placeholder?: string;
-    onChange: (e: string) => void;
+    onChange: (value:string) => void;
 }
 
-export default function InputNumber({ id, name, label, suffix, placeholder, value, onChange }: InputNumberProps) {
+export default function InputText({ id, name, label, suffix, placeholder, value, onChange }: InputTextProps) {
 
-    const handleOnChange = (value: string) => {
-        onChange(value);
+    const handleOnChange = (e:ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
     }
 
     return (
@@ -19,13 +25,13 @@ export default function InputNumber({ id, name, label, suffix, placeholder, valu
             <label htmlFor={id} className="text-preset-7 font-normal text-grey-500">{label}</label>
             <div className="border-2 border-grey-500 hover:border-blue-500 py-4 px-6 rounded-xl flex flex-row items-stretch justify-start gap-6">
                 <input
-                    type="number"
+                    type="text"
                     name={name}
                     id={id}
                     className="text-preset-4 placeholder:text-grey-500 text-blue-900 w-full h-full outline-0"
                     value={value}
                     placeholder={placeholder}
-                    onChange={(e) => handleOnChange(e.target.value)}
+                    onChange={handleOnChange}
                 />
 
                 <span className="text-preset-4 text-blue-500">
