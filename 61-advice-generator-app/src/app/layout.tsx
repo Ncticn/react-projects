360afc64b7352537b8d65b6cd76ebae696e71ba3
Ncticn <nctiicn@gmail.com>
2026-05-07 14:23:43 +0300

    Custom TailwindCSS settings have been applied. The font has been loaded and classes have been added.

--- 61-advice-generator-app/src/app/layout.tsx
+++ 61-advice-generator-app/src/app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
 import "@/src/libraries/tailwind.css";
+import { Manrope } from "next/font/google";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Advice Generator App",
   description: "The perfect project if you're learning how to interact with 3rd-party APIs. This challenge uses the Advice Slip API to generate random quotes of advice.",
 };
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-manrope"
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={`${manrope.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
