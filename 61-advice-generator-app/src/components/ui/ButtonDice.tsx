ba6bb126f118fd8f0cede161ca34e2f989fba248
Ncticn <nctiicn@gmail.com>
2026-05-07 14:37:59 +0300

    The ButtonDice component has been created and adapted to the design.

---
+++ 61-advice-generator-app/src/components/ui/ButtonDice.tsx
@@ -0,0 +1,17 @@
+import IconDice from "@/src/assets/icons/icon-dice.svg";
+
+interface ButtonDiceProps {
+    onClick?: () => void;
+}
+
+export default function ButtonDice({ onClick }: ButtonDiceProps) {
+
+    return (
+        <button
+            onClick={onClick}
+            type="button"
+            className="size-16 bg-green-300 rounded-4xl cursor-pointer transition delay-75 ease-linear hover:drop-shadow-[0_0_40px_rgba(83,255,170,1)]">
+            <img src={IconDice.src || IconDice} alt="Icon Dice" className="m-auto" />
+        </button>
+    );
+}
