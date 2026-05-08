0e5367351c40c3edceb2284dc8249b67be206f07
Ncticn <nctiicn@gmail.com>
2026-05-08 16:52:57 +0300

    The  prop has been added to the ButtonDice component.

--- 61-advice-generator-app/src/components/ui/ButtonDice.tsx
+++ 61-advice-generator-app/src/components/ui/ButtonDice.tsx
@@ -1,16 +1,17 @@
 import IconDice from "@/src/assets/icons/icon-dice.svg";
 
 interface ButtonDiceProps {
-    onClick?: () => void;
+    onClick?: () => void,
+    classList?: string,
 }
 
-export default function ButtonDice({ onClick }: ButtonDiceProps) {
+export default function ButtonDice({ onClick, classList }: ButtonDiceProps) {
 
     return (
         <button
             onClick={onClick}
             type="button"
-            className="size-16 bg-green-300 rounded-4xl cursor-pointer transition delay-75 ease-linear hover:drop-shadow-[0_0_40px_rgba(83,255,170,1)]">
+            className={`size-16 bg-green-300 rounded-4xl cursor-pointer transition delay-75 ease-linear hover:drop-shadow-[0_0_40px_rgba(83,255,170,1)] ${classList}`}>
             <img src={IconDice.src || IconDice} alt="Icon Dice" className="m-auto" />
         </button>
     );
