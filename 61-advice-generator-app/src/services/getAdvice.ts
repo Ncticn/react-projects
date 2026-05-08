233ff3e86feb0209ee2247b38dfaeca5c301d8a9
Ncticn <nctiicn@gmail.com>
2026-05-08 16:52:08 +0300

    The getAdvice service has been created and completed.

---
+++ 61-advice-generator-app/src/services/getAdvice.ts
@@ -0,0 +1,23 @@
+type Advice = {
+    id:number;
+    advice: string;
+}
+
+export default async function getAdvice(): Promise<Advice>{
+    const url = "https://api.adviceslip.com/advice";
+
+    try{
+        const response = await fetch(url, {
+            cache: "no-store"
+        });
+
+        if(!response.ok) throw new Error(`Bir Hata oluştu! HTTP Error: ${response.status}`);
+
+        const data = await response.json();
+        return data.slip;
+
+    }catch(err){
+        console.log(err);
+        throw err;
+    }
+}
