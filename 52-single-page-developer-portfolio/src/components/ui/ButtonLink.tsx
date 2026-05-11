7ec56ccec8d3d90132c3a826e6586f57fae5e621
Ncticn <nctiicn@gmail.com>
2026-05-11 21:20:07 +0300

    The ButtonLink component has been created and adapted to the design.

---
+++ 52-single-page-developer-portfolio/src/components/ui/ButtonLink.tsx
@@ -0,0 +1,19 @@
+// NextJS
+import Link from "next/link";
+
+// Props
+interface ButtonLinkProps {
+    url: string,
+    text: string,
+}
+
+export default function ButtonLink({ text, url }: ButtonLinkProps) {
+    return (
+        <Link
+            href={url}
+            className="text-base font-bold leading-6.5 tracking-[2.29px] text-base-white uppercase border-b-2 border-base-green pb-2.5 transition delay-150 ease-linear hover:text-base-green"
+        >
+            {text}
+        </Link>
+    );
+}
