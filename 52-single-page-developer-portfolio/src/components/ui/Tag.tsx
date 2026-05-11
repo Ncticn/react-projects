69e87f74fb5f23d522f22c693df2c933eb79dbd0
Ncticn <nctiicn@gmail.com>
2026-05-11 21:40:51 +0300

    The Tag component has been created and adapted to the design.

---
+++ 52-single-page-developer-portfolio/src/components/ui/Tag.tsx
@@ -0,0 +1,13 @@
+
+// Props
+interface TagProps {
+    text: string
+}
+
+export default function Tag({ text }: TagProps) {
+    return (
+        <p className="tag text-base-grey text-body uppercase text-start">
+            {text}
+        </p>
+    );
+}
