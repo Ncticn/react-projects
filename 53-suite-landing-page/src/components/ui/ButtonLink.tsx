c31f7def82de89642df99a89f93a7684d4407fde
Ncticn <nctiicn@gmail.com>
2026-05-18 18:30:21 +0300

    The ButtonLink component has been created and adapted to the design.

---
+++ 53-suite-landing-page/src/components/ui/ButtonLink.tsx
@@ -0,0 +1,17 @@
+// NextJS
+import Link from "next/link";
+
+// Props
+interface ButtonLinkProps {
+    text: string,
+    href?: string,
+    ariaLabel: string
+}
+
+export default function ButtonLink({ text, href = "#", ariaLabel }: ButtonLinkProps) {
+    return (
+        <Link href={href} aria-label={ariaLabel} className="transition-colors delay-150 ease-linear text-preset-6 text-neutral-0 hover:bg-linear-to-br hover:from-0% hover:from-[#A060FF] hover:via-50% hover:via-purple-500 hover:to-100% hover:to-[#FFA84E]  bg-neutral-900 px-8 pt-5 pb-4 rounded-md focus:outline-4 focus:outline-offset-4 focus:outline-neutral-900">
+            {text}
+        </Link>
+    );
+}
